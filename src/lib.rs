ad66e99c0dfbeb4bf0079280d9766c4518a47dc1
Sam Little <kagutaba256@gmail.com>
2020-11-29 12:06:58 -0800

    somewhat working

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use web_sys::WebGlRenderingContext as GL;
+use web_sys::*;
+
+mod gl_setup;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -16,12 +20,24 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-  let cool = format!("when ur {}-ing really hard, so hard ur yolo'd", name);
-  alert(&cool);
+pub struct Client {
+  gl: GL,
 }
 
 #[wasm_bindgen]
-pub fn say_hello_from_rust() {
-  log("howdy... from rust");
+impl Client {
+  #[wasm_bindgen(constructor)]
+  pub fn new() -> Self {
+    console_error_panic_hook::set_once();
+    let gl = gl_setup::initialize_webgl_context().unwrap();
+    Self { gl: gl }
+  }
+
+  pub fn update(&mut self, _time: f32, _height: f32, _width: f32) -> Result<(), JsValue> {
+    Ok(())
+  }
+
+  pub fn render(&self) {
+    self.gl.clear(GL::COLOR_BUFFER_BIT | GL::DEPTH_BUFFER_BIT);
+  }
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,34 @@
-import * as rust from 'wasm-2'
+import { Client } from 'wasm-2'
 
-rust.say_hello_from_rust()
+const canvas = document.getElementById('rustCanvas')
+const gl = canvas.getContext('webgl', { antialias: true })
+
+const FPS_THROTTLE = 1000.0 / 240.0
+
+const client = new Client()
+const initialTime = Date.now()
+
+let lastDrawTime = -1
+const render = () => {
+  window.requestAnimationFrame(render)
+  const currTime = Date.now()
+  if (currTime >= lastDrawTime + FPS_THROTTLE) {
+    lastDrawTime = currTime
+    if (
+      window.innerHeight != canvas.height ||
+      window.innerWidth != canvas.width
+    ) {
+      canvas.height = window.innerHeight
+      canvas.style.height = window.innerHeight
+
+      canvas.width = window.innerWidth
+      canvas.style.width = window.innerWidth
+
+      gl.viewport(0, 0, window.innerWidth, window.innerHeight)
+    }
+    let elapsedTime = currTime - initialTime
+    client.update(elapsedTime, window.innerHeight, window.innerWidth)
+    client.render()
+  }
+}
+render()
