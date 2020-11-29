ad9bff12013e22685d96aed00955b64769df0a11
Sam Little <kagutaba256@gmail.com>
2020-11-29 14:06:59 -0800

    adding shading, translation matrix, etc

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,10 @@ use wasm_bindgen::prelude::*;
 use web_sys::WebGlRenderingContext as GL;
 use web_sys::*;
 
+mod common_funcs;
 mod gl_setup;
+mod programs;
+mod shaders;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -22,6 +25,7 @@ extern "C" {
 #[wasm_bindgen]
 pub struct Client {
   gl: GL,
+  program_color_2d: programs::Color2D,
 }
 
 #[wasm_bindgen]
@@ -30,7 +34,10 @@ impl Client {
   pub fn new() -> Self {
     console_error_panic_hook::set_once();
     let gl = gl_setup::initialize_webgl_context().unwrap();
-    Self { gl: gl }
+    Self {
+      program_color_2d: programs::Color2D::new(&gl),
+      gl: gl,
+    }
   }
 
   pub fn update(&mut self, _time: f32, _height: f32, _width: f32) -> Result<(), JsValue> {
@@ -39,5 +46,14 @@ impl Client {
 
   pub fn render(&self) {
     self.gl.clear(GL::COLOR_BUFFER_BIT | GL::DEPTH_BUFFER_BIT);
+    self.program_color_2d.render(
+      &self.gl, //gl
+      0.,       //bottom
+      10.,      //top
+      0.,       //left
+      10.,      //right
+      10.,      //canvas_height
+      10.,      //canvas_width
+    );
   }
 }
